d8ef79959f250c72fc0211b797c4c74002cefde9
foxycoder1212 <samultronpowered@gmail.com>
2026-03-15 19:10:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function showMessage() {
-    document.getElementById("message").innerText =
-    "Hello! You clicked the button 🎉";
+
+    const message = document.getElementById("message");
+
+    message.innerHTML = `
+    <img src="https://picsum.photos/600/400?1" width="300"><br>
+    <img src="https://picsum.photos/600/400?2" width="300"><br>
+    <img src="https://picsum.photos/600/400?3" width="300"><br>
+    <img src="https://picsum.photos/600/400?4" width="300"><br>
+    <img src="https://picsum.photos/600/400?5" width="300">
+    `;
 }
